a1bf8dbde980bc9be8f4913a2d95590e4c67716c
thepi899 <53815508+thepi899@users.noreply.github.com>
2020-07-05 21:17:12 +0200

    Add files via upload

---
+++ Datenbank/color.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+echo.
+color 12
+timeout 1 >NULL
+color 21
+timeout 1 >Null
+color 4A
+timeout 1 >Null
+color 2B
+timeout 1 >Null
+color 15
+timeout 1 >Null
+color 16
+timeout 1 >Null
+color 17
+timeout 1 >Null
+cls 
---
+++ Datenbank/upload.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Das Backup wird erstellt
+ftp -s:upload.ftp
